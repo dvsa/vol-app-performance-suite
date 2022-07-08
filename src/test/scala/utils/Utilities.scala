4bb4ba5ccf6a4938651670ec6e25330620a8c34a
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2022-07-08 11:19:08 +0100

    removed dependency

--- src/test/scala/utils/Utilities.scala
+++ src/test/scala/utils/Utilities.scala
@@ -2,28 +2,23 @@ package utils
 
 import activesupport.config.Configuration
 import com.typesafe.config.Config
-import org.jsoup.Jsoup
-import org.jsoup.parser.Parser
+
 import utils.SetUp.env
 
 object Utilities {
   val random = new scala.util.Random
   val CONFIG: Config = new Configuration().getConfig
 
-  def htmlTableRowCounter(table: String): String = {
-    val document = Jsoup.parse(table, "", Parser.xmlParser)
-    val element = document.select("tr.row")
-    String.valueOf(element.size)
-  }
   def orderRef(): Int = random.nextInt(Integer.MAX_VALUE)
-  def randomInt(): Int = random.between(1,10)
+
+  def randomInt(): Int = random.between(1, 10)
 
   def password(): String = {
-      (env) match {
-        case "int" =>
-          CONFIG.getString("intPassword")
-        case _=>
-           "${Password}"
-      }
+    (env) match {
+      case "int" =>
+        CONFIG.getString("intPassword")
+      case _ =>
+        "${Password}"
+    }
   }
 }
