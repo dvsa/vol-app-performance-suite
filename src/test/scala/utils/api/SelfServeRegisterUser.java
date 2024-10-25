4ca240e5123c77e9b10f726c063bcac3ddbe8029
Stefan Ranoszek <stefan48500@gmail.com>
2024-10-25 11:25:42 +0100

    Revert "chore: revert"
    This reverts commit e69be69d1efdb7c622af3d34f1b2b68b6add1664.

--- src/test/scala/utils/api/SelfServeRegisterUser.java
+++ src/test/scala/utils/api/SelfServeRegisterUser.java
@@ -33,12 +33,12 @@ public class SelfServeRegisterUser {
     private String users = System.getProperty("users");
 
 
-    public static void getExternalUsersFromTable() {
+    public static void getExternalUsersFromTable(String environment, String username, String password) {
         try {
             createTunnel();
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://olcsdb-rds.int.olcs.dvsacloud.uk:3306/OLCS_RDS_OLCSDB?user={user}&password={password}?useSSL=FALSE");
+            String connectionString = String.format("jdbc:mysql://olcsdb-rds.%s.olcs.dvsacloud.uk:3306/OLCS_RDS_OLCSDB?user=%s&password=%s?useSSL=FALSE", environment, username, password);
+            Connection con = DriverManager.getConnection(connectionString);
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery(SQLquery.getUsersSql("10"));
             while (rs.next())
